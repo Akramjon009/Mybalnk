2a5d15e960747e50dece8384b04ccbec1cfad68f
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2023-12-28 11:53:21 +0500

    info: day

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,10 +1,20 @@
-﻿Console.WriteLine("kun");
-int day = int.Parse(Console.ReadLine());
-Console.WriteLine("oy");
-int oy = int.Parse(Console.ReadLine());
-Console.WriteLine("yil");
-int yil = int.Parse(Console.ReadLine());
-var dateTime = new DateTime(yil,oy,day);
+﻿int day;
+int oy;
+int yil;
+try
+{
+    Console.WriteLine("kun");
+    day = int.Parse(Console.ReadLine());
+    Console.WriteLine("oy");
+    oy = int.Parse(Console.ReadLine());
+    Console.WriteLine("yil");
+    yil = int.Parse(Console.ReadLine());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+var dateTime = new DateTime(year: yil, month: oy, day: day);
 var now = DateTime.Now;
 TimeSpan days = now - dateTime;
 Console.WriteLine(days.Days);
